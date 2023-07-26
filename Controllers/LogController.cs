d0930912e86762d8d5cf9feec88cbafc8af6a1ff
siddhraj-sinh <skrathod282001@gmail.com>
2023-07-26 12:49:54 +0530

    added ILogService and LogService

--- Controllers/LogController.cs
+++ Controllers/LogController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MinimalChatAppApi.Data;
+using MinimalChatAppApi.Interfaces;
 using Newtonsoft.Json.Linq;
 using System.Text.Json;
 using System.Text.RegularExpressions;
@@ -13,47 +14,25 @@ namespace MinimalChatAppApi.Controllers
     [ApiController]
     public class LogController : ControllerBase
     {
-        private readonly ChatContext _context;
-        public LogController(ChatContext context)
+        private readonly ILogService _logService;
+
+        public LogController(ILogService logService)
         {
-            _context = context;
+            _logService = logService;
         }
 
         [HttpGet]
-        public IActionResult GetLogs([FromQuery] DateTime? startTime = null, [FromQuery] DateTime? endTime = null)
+        public async Task<IActionResult> GetLogs([FromQuery] DateTime? startTime = null, [FromQuery] DateTime? endTime = null)
         {
-
-            var logsQuery = _context.Log.AsQueryable();
-
-            // Filter logs based on the provided start and end times, if any
-            if (startTime != null)
-            {
-                logsQuery = logsQuery.Where(l => l.Timestamp >= startTime);
-            }
-
-            if (endTime != null)
-            {
-                logsQuery = logsQuery.Where(l => l.Timestamp <= endTime);
-            }
-
-            var logs = logsQuery.ToList();
-
-            logs.ForEach(l =>
-            {
-                string cleanedString = l.RequestBody.Replace("\r\n", "").Replace(" ", "").Replace("\\","");
-                Console.WriteLine(cleanedString);
-                l.RequestBody = cleanedString;
-            });
+            var logs = await _logService.GetLogsAsync(startTime, endTime);
 
             // If no logs found based on the provided filter, return 404 Not Found
-            if (logs.Count ==0)
+            if (!logs.Any())
             {
                 return NotFound(new { error = "No logs found." });
             }
 
             return Ok(new { Logs = logs });
-
-
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -55,8 +55,9 @@ namespace MinimalChatAppApi
 
             builder.Services.AddScoped<IRepository<Message>, Repository<Message>>();
             builder.Services.AddScoped<IRepository<User>, Repository<User>>();
+            builder.Services.AddScoped<IRepository<LogModel>, Repository<LogModel>>();
             builder.Services.AddScoped<IMessageService, MessageService>();
-
+            builder.Services.AddScoped<ILogService, LogService>(); 
             builder.Services.AddHttpContextAccessor();
 
             var app = builder.Build();
